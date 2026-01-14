ee3d86f1d1dd2e0e8fcf05fad89b9dfa216243a4
Savas <savashn@proton.me>
2026-01-14 23:39:36 +0300

    upgrade ecewo to v3.1.0

--- frameworks/ecewo/main.c
+++ frameworks/ecewo/main.c
@@ -10,21 +10,30 @@ void hello_world(Req *req, Res *res) {
 int main(int argc, char *argv[]) {
   // uint8_t cpu_count = cluster_cpus_physical();
   uint8_t cpu_count = cluster_cpus();
-
   Cluster config = {
-    .cpus = cpu_count,
-    .respawn = true,
-    .port = 3000
+      .cpus = cpu_count,
+      .respawn = true,
+      .port = 3000
   };
 
-  if (cluster_init(&config, argc, argv)) {
-    cluster_wait_workers();
-    return 0;
+  int cluster_result = cluster_init(&config, argc, argv);
+  
+  // If master process (returns 0), wait for workers
+  if (cluster_result == 0) {
+      cluster_wait_workers();
+      return 0;
+  }
+  
+  // If error (returns -1 and NOT a worker), exit
+  if (cluster_result != -1 || !cluster_is_worker()) {
+      return 1;
   }
 
+  // Worker process continues here
   server_init();
   get("/", hello_world);
   server_listen(cluster_get_port());
   server_run();
+  
   return 0;
 }
